d0e911ba0d69cf4ddd89c72523f258c36427b98d
jmkim <wjdals0304@gmail.com>
2021-12-12 15:23:05 +0900

    Initial Commit

---
+++ AppleMusic/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AppleMusic
+//
+//  Created by 김정민 on 2021/12/12.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
